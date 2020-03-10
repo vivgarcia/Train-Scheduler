b746b7b4284a9167669913f7e583fd45a10fd03e
vivgarcia <viv.garcia88@gmail.com>
2020-03-10 16:20:55 -0500

    updating README.md

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Train-Scheduler
+
+link to train scheduler ['']
+
+### What Is It?
+
+Train scheduler application that incorporates Firebase to host arrival and departure data. Your app will retrieve and manipulate this information with Moment.js. This website will provide up-to-date information about various trains, namely their arrival times and how many minutes remain until they arrive at their station.
+
+### Instructions 
+
+Enter new train information, then click the submit button.
+Train information will appear and update with real-time status.
+
+
+
---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+//  TODO: Add SDKs for Firebase products that you want to use
+//  https://firebase.google.com/docs/web/setup#available-libraries -->
+
+
+  // Your web app's Firebase configuration
+    var firebaseConfig = {
+    apiKey: "AIzaSyBUJn1ZC8fjOha_GMIbjYOyfMaepE6k9h4",
+    authDomain: "train-project-vgarcia.firebaseapp.com",
+    databaseURL: "https://train-project-vgarcia.firebaseio.com",
+    projectId: "train-project-vgarcia",
+    storageBucket: "train-project-vgarcia.appspot.com",
+    messagingSenderId: "225604754998",
+    appId: "1:225604754998:web:e4f1edad1172c23ef78297"
+    };
+  // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
